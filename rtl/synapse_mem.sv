79335b4e1756ecdc70ffb5a2b1146f2b76d03dc4
ttosu25 <ttosu.dev@gmail.com>
2026-03-01 12:06:09 +0000

    Update synapse_mem.sv
    First synapse memory unit iteration

--- rtl/synapse_mem.sv
+++ rtl/synapse_mem.sv
@@ -1,7 +1,21 @@
 
 //N specifies the number of synapses in the memory unit
 
-module synapse_mem #(parameter N)
-(output signed dout[7:0], input logic addr[N-1:0], we, clk);
+
+module synapse_mem #(parameter int N)
+(output logic signed [7:0] dout, input logic signed [7:0] din , input logic [$clog2(N)-1:0]addr, input logic we, clk);
+
+	//weights for memory
+	reg signed [7:0] weights[N-1:0]; 
+	
+	always_ff@(posedge clk)
+	
+		begin
+		
+			if(we) //we = 1 is a reading operation
+				dout <= weights[addr];
+			else// we = 0 is a writing operation
+				weights[addr] <= din;	
+		end
 
 endmodule
