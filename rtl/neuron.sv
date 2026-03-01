f9f1cdbb345a19463b5194ebb19ccac003cd0c04
ttosu25 <ttosu.dev@gmail.com>
2026-03-01 12:30:44 +0000

    Create neuron.sv

---
+++ rtl/neuron.sv
@@ -0,0 +1,8 @@
+
+neuron #(paramater M) // M is the number of membranes and thus also the number of neurons
+(output logic spike, input logic [$clog2(M)-1:0]addr, input logic signed [7:0]threshold, input logic signed [7:0] leak, input logic signed [7:0] V_reset, clk);
+
+ // Using the equation specified in the documentation
+	
+
+endmodule
